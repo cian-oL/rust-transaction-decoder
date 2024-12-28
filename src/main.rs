957dc68435f2abb267029bd033e8c9283c678b6c
mecastor <mecastor87@gmail.com>
2024-12-28 19:35:25 +0000

    Refactored to remove error handling from main fn & updated unit tests

--- src/main.rs
+++ src/main.rs
@@ -72,9 +72,7 @@ fn hash_raw_transaction(raw_transaction: &[u8]) -> Txid {
     Txid::from_bytes(hash2.into())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
-
+fn decode(transaction_hex: String) -> Result<String, Box<dyn Error>> {
     let transaction_bytes =
         hex::decode(transaction_hex).map_err(|e| format!("Hex decode error: {}", e))?;
     let mut bytes_slice = transaction_bytes.as_slice();
@@ -127,40 +125,48 @@ fn main() -> Result<(), Box<dyn Error>> {
         lock_time,
     };
 
-    println!(
-        "Transaction {}",
-        serde_json::to_string_pretty(&transaction)?
-    );
-    Ok(())
+    Ok(serde_json::to_string_pretty(&transaction)?)
+}
+
+fn main() {
+    let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
+
+    match decode(transaction_hex.to_string()) {
+        Ok(json) => println!("{}", json),
+        Err(e) => println!("{}", e),
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::read_compact_size;
+    use super::Error;
 
     #[test]
-    fn test_read_compact_size() {
+    fn test_read_compact_size() -> Result<(), Box<dyn Error>> {
         let mut bytes = [1_u8].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 1_u64);
 
         let mut bytes = [253_u8, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64);
 
         let mut bytes = [254_u8, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(3));
 
         let mut bytes = [255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(7));
 
         let hex = "fd204e";
         let decoded = hex::decode(hex)?;
         let mut bytes = decoded.as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         let expected_count = 20_000_u64;
         assert_eq!(count, expected_count);
+
+        Ok(())
     }
 }
